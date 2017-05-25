6a5df3f49544458969939b594569a9d5e2f2e3da
Bhushan Vaiude <bhushan.v@peopleinteractive.in>
2017-05-25 13:47:40 +0530

    proper flow added

---
+++ service/DeviceDetection.php
@@ -0,0 +1,25 @@
+<?php
+class DeviceDetection
+{
+
+	public function getBrowser()
+	{
+	}
+
+	public function getDevice()
+	{
+	}
+
+	public function isMobile()
+	{
+	}
+
+	public function isMobileApp()
+	{
+	}
+	public function getOS()
+	{
+	}
+}
+
+?>
